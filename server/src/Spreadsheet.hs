147850592bf75ce90227ce9bdb5b00bc3cac6621
hassanAlAwwadi <hassan.awwadi@gmail.com>
2024-03-12 20:25:21 +0100

    removed type families, added some graph stuff
    still need to do graph chasing, and might want to do forward edges instead of back edges like I store now...

--- server/src/Spreadsheet.hs
+++ server/src/Spreadsheet.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE TypeData, TypeFamilies, AllowAmbiguousTypes #-}
+{-# LANGUAGE LambdaCase #-}
 
 module Spreadsheet where
 import Spreadsheet.Input
@@ -9,24 +10,48 @@ import Data.List(foldl')
 import qualified Data.Map as M
 
 -- Placeholders
-type Arr   = Map (Int, Int) Formula
+type Arr   = Map (Int, Int) (Formula, Int)
 type Graph = Map (Int, Int) [(Int, Int)]
 
 data Spreadsheet = S Arr Graph 
 
-class Handle a where 
-  handle :: INPUT_LOC a -> INPUT_REP a -> Spreadsheet -> Spreadsheet 
+handle :: Input -> Spreadsheet -> Spreadsheet 
+handle i (S a g) = let 
+  g' = updateGraph i g 
+  a' = updateArr i a 
+  in S a' g'
 
-instance Handle (Aether) where 
-  handle () () = id 
+updateArr   :: Input  -> Arr -> Arr 
+updateArr (Aether) a       = a
+updateArr (Cell (x,y) v) a = M.insert (x,y) (v, eval a (x,y) v) a
 
-instance Handle (Cell) where 
-  handle (x,y) v (S a g) = S (M.insert (x,y) v a) g
+updateGraph :: Input -> Graph -> Graph 
+updateGraph Aether g = g 
+updateGraph (Cell (x,y) v) g = case v of 
+  Raw _           -> M.insert (x,y) [] g
+  Reference x' y' -> M.insert (x,y) [(locate x x', locate y y')] g
+  Plus l r        -> let 
+    start = M.insert (x,y) [] g 
+    in gentleUpdate r $ gentleUpdate l start  where 
+      gentleUpdate (Raw _) graph           = graph
+      gentleUpdate (Plus l' r') graph      = gentleUpdate r'  $ gentleUpdate l' graph
+      gentleUpdate (Reference x' y') graph = M.alter 
+        (\case Nothing  -> Just ([(locate x x', locate y y')])
+               Just ls  -> Just ((locate x x', locate y y') : ls)) 
+        (x,y) 
+        graph 
 
-instance Handle (Table) where 
-  handle ((t,l), (b,r)) vvs (S a g) = let 
-    x_keyed   = map (zip [l..r]) vvs  -- [[(x,v)]]
-    x_y_keyed = zipWith (\y vs -> zip (repeat y) vs) [t..b] x_keyed -- [[(y,(x,v))]]
-    collapsed = concat x_y_keyed -- [(y,(x,v))]
-    arr = foldl' (\m (y,(x, v)) -> M.insert (x,y) v m) a collapsed
-    in S arr g 
+eval :: Arr -> (Int, Int) -> Formula -> Int
+eval _ _ (Raw i)                  = i
+eval arr (x,y) (Reference x' y')  = let 
+  (nx,ny) = (locate x x', locate y y')
+  (_, r) = arr M.! (nx, ny) 
+  in r
+eval arr (x,y) (Plus l r) = let 
+  l' = eval arr (x,y) l
+  r' = eval arr (x,y) r 
+  in l' + r' 
+
+locate :: Int -> Ref -> Int 
+locate _ (Loc i)           = i 
+locate (lodestone) (Rel i) = lodestone + i
--- server/src/Spreadsheet/Unit.hs
+++ server/src/Spreadsheet/Unit.hs
@@ -2,4 +2,10 @@ module Spreadsheet.Unit where
 
 data Formula 
   = Raw Int 
+  | Reference Ref Ref
+  | Plus Formula Formula
   -- | Reference Int Int -- Will need a more complex constructor for this, I fear.
+
+data Ref 
+  = Loc Int 
+  | Rel Int 
