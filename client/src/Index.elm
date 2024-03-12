809e5ef71e959573f96f30ca4f2e8a190df8e888
hassanAlAwwadi <hassan.awwadi@gmail.com>
2024-03-12 17:06:51 +0100

    some more elm stuff

--- client/src/Index.elm
+++ client/src/Index.elm
@@ -1,5 +1,38 @@
 module Index exposing (..)
 import Html
-    
+import Array as A
+
 main =
   Html.text "Hello!"
+
+
+type alias Model =
+  { max_x  : Int 
+  , max_y  : Int
+  , values : A.Array (A.Array (Maybe Int))
+  }
+
+
+init : Model
+init =
+  Model 100 100 (A.initialize 100 (\_ -> A.initialize 100  (\_ -> Nothing)))
+
+type alias Msg = 
+  { x : Int
+  , y : Int 
+  , v : Maybe Int
+  }
+
+
+update : Msg -> Model -> Model
+update ({ x, y, v }) ({max_x, max_y, values} as m) = 
+  let mx  = A.get x values
+  in 
+  case mx of 
+    Nothing -> m
+    Just xr -> 
+      let 
+        xrp = A.set y v xr
+        vlp = A.set x xrp values
+      in Model max_x max_y vlp
+
--- server/app/Main.hs
+++ server/app/Main.hs
@@ -1,8 +1,4 @@
-{-# LANGUAGE OverloadedStrings #-}
-module Main where
-import API 
-import Network.Wai.Handler.Warp
-import qualified Spreadsheet as S()
+module Main where 
 
-main :: IO ()
-main = run 8081 app1
+
+main = pure ()
