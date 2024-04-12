05af2c0923a42c032569e3184486f64d0f87b851
hassanAlAwwadi <hassan.awwadi@gmail.com>
2024-04-12 14:39:16 +0200

    parser changes to make some things less fragile, perhaps

--- server/src/Formula/Parser.hs
+++ server/src/Formula/Parser.hs
@@ -15,28 +15,49 @@ un_sym_table :: [(String, (Int -> Int))]
 un_sym_table = [("(-)", negate)]
 
 fParser :: P.ReadP (Formula Int)
-fParser = P.skipSpaces *> P.choice
-  [ Raw <$> read @Int <$> P.munch1 C.isDigit
-  , Ref
-      <$ P.char 'c' <*> tParser
-      <* P.char 'r' <*> tParser
-  , flip Ref
-      <$ P.char 'r' <*> tParser
-      <* P.char 'c' <*> tParser
-  , Un
+fParser = P.skipSpaces *>
+  (    P.char '(' *> fParser <* P.char ')'
+  P.<++ Raw <$> read @Int <$> P.munch1 C.isDigit
+  -- very fragile
+  P.<++ Ref
+      <$ P.char 'c' <*> tParserC
+      <* P.char '-' 
+      <* P.char 'r' <*> tParserR
+  P.<++ flip Ref
+      <$ P.char 'r' <*> tParserR
+      <* P.char '-' 
+      <* P.char 'c' <*> tParserC
+  P.<++ Un
     <$> uParser
     <*> fParser
-  , Op
+  P.<++ Op
     <$> bParser
     <*> fParser
     <*> fParser
-  ] where
-    tParser = P.choice
+  ) where
+    tParserR = P.choice
       [ Loc <$> read @Int <$> P.munch1 C.isDigit
-      , Rel <$ P.char '$' <*> (read @Int <$> P.munch1 C.isDigit)
+      , Rel <$ P.char '$' <*> (P.choice [id <$ P.char '+', (*(-1)) <$ P.char '-'] <*> (read @Int <$> P.munch1 C.isDigit))
       ]
+    tParserC = P.choice
+      [ Loc <$> base26tobase10 <$> P.munch1 C.isAlpha
+      , Rel <$ P.char '$' <*> (P.choice [id <$ P.char '+', (*(-1)) <$ P.char '-'] <*> (read @Int <$> P.munch1 C.isDigit))
+      ]
+    base26tobase10 :: String -> Int 
+    base26tobase10 = go 0 where 
+      go n ['a'] = n -- special exception for a's at the last spot, where for some reason they represent a 0
+      go n [] = n 
+      go n (s:tr) = case base26 M.!? s  of 
+        Just v  -> go (n*26 + v) tr
+        Nothing -> n 
+
+
     bParser = P.skipSpaces *> P.choice (map (\(k, f) -> P.string k *> pure f) bin_sym_table)
     uParser = P.skipSpaces *> P.choice (map (\(k, f) -> P.string k *> pure f) un_sym_table )
+
+-- | trying to make sure it only gets calculated once so expelling it from the where
+base26 :: Map Char Int
+base26 = M.fromList $ zip ['a' .. 'z'] [1 .. 26]
 
 parseFormula :: String -> Maybe (Formula Int)
 parseFormula s =
